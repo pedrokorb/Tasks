b7bb1b21320a78103aa97b54acc6f6f8fd2b821f
pedrokorb <pedro.korb@gmail.com>
2019-11-30 16:32:37 -0300

    add task component

--- src/screens/Agenda.js
+++ src/screens/Agenda.js
@@ -4,6 +4,7 @@ import moment from 'moment'
 import 'moment/locale/pt-br'
 import todayImage from '../../assets/imgs/today.jpg'
 import commonStyles from '../commonStyles'
+import Task from '../components/Task'
 
 export default class Agenda extends Component {
   render() {
@@ -16,9 +17,17 @@ export default class Agenda extends Component {
           </View>
         </ImageBackground>
         <View style={styles.taskContainer}> 
-          <Text>Tarefa 1</Text>
-          <Text>Tarefa 2</Text>
-          <Text>Tarefa 3</Text>
+          <Task
+            desc='Tarefa Pendente'
+            estimateAt={new Date()}
+            doneAt={null}
+          />
+
+          <Task
+            desc='Tarefa Concluída'
+            estimateAt={new Date()}
+            doneAt={new Date()}
+          />
         </View>
       </View>
     )
