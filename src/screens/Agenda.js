e15796ef914fa70b2396a604a7ce558d8cffae31
Guilherme Muller <gui.msr.98@gmail.com>
2020-10-31 19:15:11 -0300

    add prettier format to all files

--- src/screens/Agenda.js
+++ src/screens/Agenda.js
@@ -1,85 +1,185 @@
-import React, { Component } from 'react'
-import { 
+import React, {Component} from 'react';
+import {
   StyleSheet,
   Text,
   View,
   ImageBackground,
   FlatList,
   TouchableOpacity,
-  Platform
-  } from 'react-native'
-import moment from 'moment'
-import 'moment/locale/pt-br'
-import todayImage from '../../assets/imgs/today.jpg'
-import commonStyles from '../commonStyles'
-import Task from '../components/Task'
-import Icon from 'react-native-vector-icons/FontAwesome'
+  Platform,
+} from 'react-native';
+import moment from 'moment';
+import 'moment/locale/pt-br';
+import todayImage from '../../assets/imgs/today.jpg';
+import commonStyles from '../commonStyles';
+import Task from '../components/Task';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default class Agenda extends Component {
   state = {
     tasks: [
-      { id: Math.random(), desc: 'Tarefa Concluída', estimateAt: new Date(), doneAt: new Date()},
-      { id: Math.random(), desc: 'Tarefa Pendente', estimateAt: new Date(), doneAt: null },
-      { id: Math.random(), desc: 'Tarefa Concluída', estimateAt: new Date(), doneAt: new Date() },
-      { id: Math.random(), desc: 'Tarefa Pendente', estimateAt: new Date(), doneAt: null },
-      { id: Math.random(), desc: 'Tarefa Concluída', estimateAt: new Date(), doneAt: new Date() },
-      { id: Math.random(), desc: 'Tarefa Pendente', estimateAt: new Date(), doneAt: null },
-      { id: Math.random(), desc: 'Tarefa Concluída', estimateAt: new Date(), doneAt: new Date() },
-      { id: Math.random(), desc: 'Tarefa Pendente', estimateAt: new Date(), doneAt: null },
-      { id: Math.random(), desc: 'Tarefa Concluída', estimateAt: new Date(), doneAt: new Date() },
-      { id: Math.random(), desc: 'Tarefa Pendente', estimateAt: new Date(), doneAt: null },
-      { id: Math.random(), desc: 'Tarefa Concluída', estimateAt: new Date(), doneAt: new Date() },
-      { id: Math.random(), desc: 'Tarefa Pendente', estimateAt: new Date(), doneAt: null },
-      { id: Math.random(), desc: 'Tarefa Concluída', estimateAt: new Date(), doneAt: new Date() },
-      { id: Math.random(), desc: 'Tarefa Pendente', estimateAt: new Date(), doneAt: null },
-      { id: Math.random(), desc: 'Tarefa Concluída', estimateAt: new Date(), doneAt: new Date() },
-      { id: Math.random(), desc: 'Tarefa Pendente', estimateAt: new Date(), doneAt: null },
-      { id: Math.random(), desc: 'Tarefa Concluída', estimateAt: new Date(), doneAt: new Date() },
-      { id: Math.random(), desc: 'Tarefa Pendente', estimateAt: new Date(), doneAt: null },
-      { id: Math.random(), desc: 'Tarefa Concluída', estimateAt: new Date(), doneAt: new Date() },
-      { id: Math.random(), desc: 'Tarefa Pendente', estimateAt: new Date(), doneAt: null },
+      {
+        id: Math.random(),
+        desc: 'Tarefa Concluída',
+        estimateAt: new Date(),
+        doneAt: new Date(),
+      },
+      {
+        id: Math.random(),
+        desc: 'Tarefa Pendente',
+        estimateAt: new Date(),
+        doneAt: null,
+      },
+      {
+        id: Math.random(),
+        desc: 'Tarefa Concluída',
+        estimateAt: new Date(),
+        doneAt: new Date(),
+      },
+      {
+        id: Math.random(),
+        desc: 'Tarefa Pendente',
+        estimateAt: new Date(),
+        doneAt: null,
+      },
+      {
+        id: Math.random(),
+        desc: 'Tarefa Concluída',
+        estimateAt: new Date(),
+        doneAt: new Date(),
+      },
+      {
+        id: Math.random(),
+        desc: 'Tarefa Pendente',
+        estimateAt: new Date(),
+        doneAt: null,
+      },
+      {
+        id: Math.random(),
+        desc: 'Tarefa Concluída',
+        estimateAt: new Date(),
+        doneAt: new Date(),
+      },
+      {
+        id: Math.random(),
+        desc: 'Tarefa Pendente',
+        estimateAt: new Date(),
+        doneAt: null,
+      },
+      {
+        id: Math.random(),
+        desc: 'Tarefa Concluída',
+        estimateAt: new Date(),
+        doneAt: new Date(),
+      },
+      {
+        id: Math.random(),
+        desc: 'Tarefa Pendente',
+        estimateAt: new Date(),
+        doneAt: null,
+      },
+      {
+        id: Math.random(),
+        desc: 'Tarefa Concluída',
+        estimateAt: new Date(),
+        doneAt: new Date(),
+      },
+      {
+        id: Math.random(),
+        desc: 'Tarefa Pendente',
+        estimateAt: new Date(),
+        doneAt: null,
+      },
+      {
+        id: Math.random(),
+        desc: 'Tarefa Concluída',
+        estimateAt: new Date(),
+        doneAt: new Date(),
+      },
+      {
+        id: Math.random(),
+        desc: 'Tarefa Pendente',
+        estimateAt: new Date(),
+        doneAt: null,
+      },
+      {
+        id: Math.random(),
+        desc: 'Tarefa Concluída',
+        estimateAt: new Date(),
+        doneAt: new Date(),
+      },
+      {
+        id: Math.random(),
+        desc: 'Tarefa Pendente',
+        estimateAt: new Date(),
+        doneAt: null,
+      },
+      {
+        id: Math.random(),
+        desc: 'Tarefa Concluída',
+        estimateAt: new Date(),
+        doneAt: new Date(),
+      },
+      {
+        id: Math.random(),
+        desc: 'Tarefa Pendente',
+        estimateAt: new Date(),
+        doneAt: null,
+      },
+      {
+        id: Math.random(),
+        desc: 'Tarefa Concluída',
+        estimateAt: new Date(),
+        doneAt: new Date(),
+      },
+      {
+        id: Math.random(),
+        desc: 'Tarefa Pendente',
+        estimateAt: new Date(),
+        doneAt: null,
+      },
     ],
     visibleTasks: [],
     showDoneTasks: true,
-  }
+  };
 
   filterTasks = () => {
-    let visibleTasks = null
-    if (this.state.showDoneTasks){
-      visibleTasks = [...this.state.tasks]
+    let visibleTasks = null;
+    if (this.state.showDoneTasks) {
+      visibleTasks = [...this.state.tasks];
     } else {
-      const pending = task => task.doneAt === null
-      visibleTasks = this.state.tasks.filter(pending)
+      const pending = (task) => task.doneAt === null;
+      visibleTasks = this.state.tasks.filter(pending);
     }
-    this.setState({ visibleTasks })
-  }
+    this.setState({visibleTasks});
+  };
 
   toggleFilter = () => {
-    this.setState({ showDoneTasks: !this.state.showDoneTasks }, this.filterTasks)
-  }
+    this.setState({showDoneTasks: !this.state.showDoneTasks}, this.filterTasks);
+  };
 
   componentDidMount = () => {
-    this.filterTasks()
-  }
+    this.filterTasks();
+  };
 
-  toggleTask = id => {
-    const tasks = this.state.tasks.map(task => {
+  toggleTask = (id) => {
+    const tasks = this.state.tasks.map((task) => {
       if (task.id === id) {
-        task = {...task}
-        task.doneAt = task.doneAt ? null : new Date()
+        task = {...task};
+        task.doneAt = task.doneAt ? null : new Date();
       }
-      return task
-    })
-    this.setState({ tasks }, this.filterTasks)
-  }
-  
+      return task;
+    });
+    this.setState({tasks}, this.filterTasks);
+  };
+
   render() {
     return (
       <View style={styles.container}>
         <ImageBackground source={todayImage} style={styles.background}>
           <View style={styles.iconBar}>
             <TouchableOpacity onPress={this.toggleFilter}>
-              <Icon 
+              <Icon
                 name={this.state.showDoneTasks ? 'eye' : 'eye-slash'}
                 size={20}
                 color={commonStyles.colors.secondary}
@@ -87,19 +187,23 @@ export default class Agenda extends Component {
             </TouchableOpacity>
           </View>
           <View style={styles.titleBar}>
-            <Text style={styles.title}>Hoje</Text>  
-            <Text style={styles.subtitle}>{moment().locale('pt-br').format('ddd, D [de] MMMM')}</Text>
+            <Text style={styles.title}>Hoje</Text>
+            <Text style={styles.subtitle}>
+              {moment().locale('pt-br').format('ddd, D [de] MMMM')}
+            </Text>
           </View>
         </ImageBackground>
-        <View style={styles.taskContainer}> 
-          <FlatList 
+        <View style={styles.taskContainer}>
+          <FlatList
             data={this.state.visibleTasks}
-            keyExtractor={item => `${item.id}`}
-            renderItem={({item}) => <Task {...item} toggleTask={this.toggleTask} />}
+            keyExtractor={(item) => `${item.id}`}
+            renderItem={({item}) => (
+              <Task {...item} toggleTask={this.toggleTask} />
+            )}
           />
         </View>
       </View>
-    )
+    );
   }
 }
 
@@ -137,4 +241,4 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'flex-end',
   },
-})
+});
